a1174d160fd2cb5c7ce1256d8e8116fc571fd5c4
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-12-01 01:06:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const cors = require("cors");
-const socketIO = require("socket.io")(http, {
-  cors: {
-    origin: "http://10.0.2.2:3000/",
-  },
-});
+const socketIO = require("socket.io")(http);
 
 const PORT = 4000;
 
